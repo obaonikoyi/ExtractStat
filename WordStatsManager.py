f5cc4977c4841f09e059ebd7d4699165e795ddc3
Obadiah Onikoyi <obaonikoyi@gmail.com>
2022-06-23 07:08:18 +0930

    update

--- WordStatsManager.py
+++ WordStatsManager.py
@@ -1,6 +1,4 @@
-
-
-    
+from distutils import text_file
 import os
 from FileAccess import *
 from Formatting import *
@@ -11,22 +9,26 @@ from Tokenising import *
 class WordStatsManager:
 
     def print_summary(number_specified, output_spec, input_paths):
-        for path in input_paths:
-            file_cotent = TextFileAccess.get_file_content(path)
-            token = StringTokenizer.split_text_to_token(file_cotent)
-            number_of_words = SummaryStatistics.number_of_words(token)
-            most_frequent_words = SummaryStatistics.most_frequent_words(token, number_specified)
+        for output in output_spec:
 
-            def get_format_from_path(path):
-                _base, extension = os.path.splitext(path)
-                return extension.lower()
+            for path in input_paths:
+                file_cotent = TextFileAccess.get_file_content(path)
+                token = StringTokenizer.split_text_to_token(file_cotent)
+                number_of_words = SummaryStatistics.number_of_words(token)
+                most_frequent_words = SummaryStatistics.most_frequent_words(token, number_specified)
 
+            
             format = get_format_from_path(path)
             if format == '.csv' :
-                text_result = CSVFileFormat.format_file_summary(path, number_of_words,most_frequent_words)
-            else: 
-                text_result = TextFileFormat.format_file_summary(path, number_of_words,most_frequent_words)
+                result += CSVFileFormat.format_file_summary(path, number_of_words,most_frequent_words)
+            if format == '.text' : 
+                result += TextFileFormat.format_file_summary(path, number_of_words,most_frequent_words)
 
-            TextFileAccess.open_write(text_result)
-            print(text_result)
-                   
+
+            TextFileAccess.open_write(output, result)
+            print(result)
+
+
+def get_format_from_path(path):
+        _base, extension = os.path.splitext(path)
+        return extension.lower()
